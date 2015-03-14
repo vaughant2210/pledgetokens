2ae13a36ae72105048b44cff6934410264ee026e
Vaughan Thandroyen <vaughant2210@gmail.com>
2015-03-14 17:19:46 -0400

    add Parse model in tokens route

--- app/routes/tokens.js
+++ app/routes/tokens.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model:function (){
-
+      return Ember.$.ajax("https://api.parse.com/1/classes/token").then(function(response){
+        return response.results;
+      });
   }
 });
